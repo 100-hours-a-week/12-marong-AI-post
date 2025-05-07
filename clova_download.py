ef476a36fa091d56a2a419c9c24eb0db9d46094f
kkuriyoon <kkuriyoon5007@gmail.com>
2025-05-07 16:29:53 +0900

    hf_token

--- clova_download.py
+++ clova_download.py
@@ -8,8 +8,8 @@ hf_token = os.getenv("HF_TOKEN")
 model_name = "naver-hyperclovax/HyperCLOVAX-SEED-Text-Instruct-1.5B"
 save_path = "models/hyperclovax-1.5b-instruct"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=hf_token)
+tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_token)
 tokenizer.save_pretrained(save_path)
 
-model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=hf_token)
+model = AutoModelForCausalLM.from_pretrained(model_name, token=hf_token)
 model.save_pretrained(save_path)
