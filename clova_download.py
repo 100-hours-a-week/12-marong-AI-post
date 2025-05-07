e8a1cee71742341fb9a818403a7c4b46ba485be3
kkuriyoon <kkuriyoon5007@gmail.com>
2025-05-07 09:09:00 +0900

    hf_token 수정

---
+++ clova_download.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+load_dotenv()
+hf_token = os.getenv("HF_TOKEN")
+
+model_name = "naver-hyperclovax/HyperCLOVAX-SEED-Text-Instruct-1.5B"
+save_path = "models/hyperclovax-1.5b-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=hf_token)
+tokenizer.save_pretrained(save_path)
+
+model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=hf_token)
+model.save_pretrained(save_path)
